d6c34e388c21bf048c6650c11669e36ec8bf046a
Jiří Vrba <jiri@vrba.dev>
2023-12-05 06:14:09 +0100

    Solve day 4, part 2

--- src/main/kotlin/dev/vrba/aoc/Day04.kt
+++ src/main/kotlin/dev/vrba/aoc/Day04.kt
@@ -1,5 +1,6 @@
 package dev.vrba.aoc
 
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.math.pow
 
 object Day04 : Task(4) {
@@ -9,22 +10,47 @@ object Day04 : Task(4) {
         val winningNumbers: Set<Int>,
         val scratchedNumbers: List<Int>
     ) {
+        val matchingNumbers: Int
+            get() = scratchedNumbers.count { it in winningNumbers }
+
         val points: Int
-            get() = 2.0.pow(scratchedNumbers.count { it in winningNumbers } - 1).toInt()
+            get() = 2.0.pow(matchingNumbers - 1).toInt()
     }
 
     override fun part1(): Int =
         parseInput().sumOf { it.points }
 
-    private fun parseInput(): List<ScratchCard> =
-        readInputLines().map { line ->
+    override fun part2(): Int {
+        val initialScratchcards = parseInput()
+
+        val scratchcards = initialScratchcards.toMutableList()
+        val processedScratchcards = AtomicInteger()
+
+        while (scratchcards.isNotEmpty()) {
+            val current = scratchcards.first()
+            val gainedScratchcards = initialScratchcards.drop(current.number).take(current.matchingNumbers)
+
+            processedScratchcards.incrementAndGet()
+
+            scratchcards.removeAt(0)
+            scratchcards.addAll(0, gainedScratchcards)
+        }
+
+        return processedScratchcards.get()
+    }
+
+    private fun parseInput(): List<ScratchCard> {
+        fun String.parseNumbers(): List<Int> = trim().split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+
+        return readInputLines().map { line ->
             val (number, numbers) = line.removePrefix("Card ").split(": ")
             val (winningNumbers, scratchedNumbers) = numbers.split(" | ")
 
             ScratchCard(
                 number = number.trim().toInt(),
-                winningNumbers = winningNumbers.trim().split(" ").filter { it.isNotBlank() }.map { it.toInt() }.toSet(),
-                scratchedNumbers = scratchedNumbers.trim().split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+                winningNumbers = winningNumbers.parseNumbers().toSet(),
+                scratchedNumbers = scratchedNumbers.parseNumbers()
             )
         }
+    }
 }
