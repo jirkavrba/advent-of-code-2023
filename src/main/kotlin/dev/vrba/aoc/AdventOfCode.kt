f7a84134ef69b567a72e4d4dba76c4cebf13c4ae
Jiří Vrba <jiri@vrba.dev>
2023-12-06 22:33:59 +0100

    Solve day 6, part 1

--- src/main/kotlin/dev/vrba/aoc/AdventOfCode.kt
+++ src/main/kotlin/dev/vrba/aoc/AdventOfCode.kt
@@ -4,16 +4,4 @@ object AdventOfCode
 
 fun main() {
     println("\uD83C\uDF84 Advent of code 2023 \uD83C\uDF84")
-
-    val solvedDays = listOf(
-//        Day01,
-//        Day02,
-//        Day03,
-//        Day04,
-        Day05
-    )
-
-    solvedDays.forEach {
-        it.display()
-    }
 }
